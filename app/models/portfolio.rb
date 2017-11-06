d4b142c99084d11fd82a33993539d1412c58848c
Mario Florence <mflorence1989@gmail.com>
2017-11-05 22:32:50 -0500

    implent default images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,4 +5,11 @@ def self.angular
   end
 
 scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Rails') }
+
+after_initialize :set_defaults
+
+def set_defaults
+  self.main_image ||= "http://via.placeholder.com/600x400"
+  self.thumb_image ||= "http://via.placeholder.com/350x200"
+end
 end
