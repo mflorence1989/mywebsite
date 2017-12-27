a27eca9aa929d93782d65a665e8eaf75c8c5ebdc
Mario Florence <mflorence1989@gmail.com>
2017-12-26 23:02:46 -0500

    integerate florence view tool gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,7 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  def copyright_generator
+    FlorenceViewTool::Renderer.copyright 'Mario Florence', 'All rights reserved'
+    end
 end
