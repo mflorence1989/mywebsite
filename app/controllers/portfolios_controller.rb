4deec9fe9dfa0724c13efa3aca38db4c8ab9ced7
Mario Florence <mflorence1989@gmail.com>
2018-01-05 12:31:33 -0500

    trying to fix bug

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,18 +1,19 @@
 class PortfoliosController < ApplicationController
-before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
-  layout "portfolio"
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
+
   def index
     @portfolio_items = Portfolio.by_position
   end
 
   def sort
-   params[:order].each do |key, value|
-     Portfolio.find(value[:id]).update(position: value[:position])
-   end
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
 
-   render nothing: true
-end
+    render nothing: true
+  end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
@@ -36,12 +37,9 @@ end
   end
 
   def edit
-
   end
 
   def update
-
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -52,13 +50,9 @@ end
   end
 
   def show
-
   end
 
   def destroy
-    # Perform the lookup
-
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -77,8 +71,8 @@ end
                                       technologies_attributes: [:name]
                                      )
   end
-  def set_portfolio_item
-  @portfolio_item = Portfolio.find(params[:id])
-  end
 
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
