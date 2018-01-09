c7edcb59208b7d5e29a918c3395ae2a9928dfc60
Mario Florence <mflorence1989@gmail.com>
2018-01-09 15:20:53 -0500

    update source tracking feat

--- app/controllers/concerns/set_source.rb
+++ app/controllers/concerns/set_source.rb
@@ -1,13 +1,11 @@
 module SetSource
-extend ActiveSupport::Concern
-included do
-before_action :set_source
+  extend ActiveSupport::Concern
 
-end
+  included do
+    before_action :set_source
+  end
 
-def set_source
-session[:source] = params[:q] if params[:q]
-end
-
-
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
